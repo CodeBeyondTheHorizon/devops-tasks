4bc21c94ca04f01684daf1e4317a1bd01352630f
Coder <test@gmail.com>
2025-09-26 20:25:37 +0600

    Task3.Done

---
+++ TASK3_Output/task3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Hello ! Please enter your email: "
+read email
+
+echo "Enter your password:"
+read password
+
+echo "Thank you! Your email is $email"
+echo "Your password is $password"
+
+# End of script
+
+# To run please use the command: bash TASK3_Output/task3.sh
